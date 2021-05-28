4c750cc21e5703bab6e2081796883682bef1f604
Alex Samokhin <aleksey.samohin@leroymerlin.ru>
2021-05-28 17:40:09 +0300

    generate nest app

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Office Map')
+    .setDescription('Office map API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
